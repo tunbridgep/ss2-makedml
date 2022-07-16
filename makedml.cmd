2ce97aa0d6fa81572dd21193d35e84751124d99e
Paul Tunbridge <tunbridgep@gmail.com>
2022-07-16 22:36:25 +1000

    Huge cleanup. Added support for # folders for both features and versions.

--- makedml.cmd
+++ makedml.cmd
@@ -8,8 +8,31 @@
 
 ::Set third parameter to 1 to enable DML1 header generation
 ::This is useful if you're not using a $common folder, so DML files can be written in any order.
-call :make_features %1 %2 %3
-::call :make_versions %1 %2
+if "%1" == "-f" (
+	call :make_features %2 %3 %4
+) else if "%1" == "-v" (
+	call :make_versions %2 %3
+) else (
+	echo MakeDML version 0.1
+	echo:
+	echo usage:
+	echo for feature mode
+	echo 	makedml.cmd -f $source_dir $destination_dir $make_DML_headers
+	echo or for version mode
+	echo 	makedml.cmd -v $source_dir $destination_dir
+	echo:
+	echo In feature mode, the structure of each subfolder of $source_dir will be copied into $destination_dir in sequence.
+	echo Files that exist in multiple folders will be concatenated together with a new line.
+	echo Setting the $make_DML_headers variable to 1 will add "DML1" to the first line of every .dml file found.
+	echo This is because the DML header must appear at the top of all DML files, but feature folders can be in any order.
+	echo If you wish to specify your own DML headers, they should be in
+	echo a folder that appears at the TOP of your src folder, it's recommended to use $core or something similar.
+	echo In that instance, the $make_DML_headers variable should be 0 or not specified.
+	echo:
+	echo In version mode, for each subfolder of $source_dir, the contents of $common will be copied into a new subfolder in $destination_dir.
+	echo Then, the contents of the subfolder in $source_dir will be added to the equivalent subfolder in $destination_dir.
+	echo Files that exist in both folders will be concatenated together with a new line.
+)
 
 EXIT /B
 
@@ -34,37 +57,45 @@ mkdir "%~dpnx2"
 ::for each feature folder, copy each file to the dest folder, appending if required
 for /D %%i in ("%~dpnx1\*") do (
 
-	::This is some hacked together magic
-	setlocal disableDelayedExpansion
-	for /f "delims=" %%A in ('forfiles /P "%%i" /s /m *.* /c "cmd /c echo @relpath"') do (
-		set "file=%%~A"
-		set "ext=%%~xA"
-		setlocal enableDelayedExpansion
-		set "file=!file:~2!"
-		
-		set first_character=!file:~0,1!
-		if NOT "!first_character!" == "#" (
-				
-			echo Writing file "%~dpnx2\!file!"		
-			if "%3" == "1" (
-				if !ext! == .dml (
-					if not exist "%~dpnx2\!file!" (
-						echo new DML file - writing DML1 header
-						echo DML1> "%~dpnx2\!file!"
+	setlocal enableDelayedExpansion
+	set "folder_name=%%~nxi"
+	if "!folder_name:~0,1!" == "#" (
+		echo skipping ignored feature !folder_name!
+	) else (
+		echo adding feature !folder_name!
+		::This is some hacked together magic
+		setlocal disableDelayedExpansion
+		for /f "delims=" %%A in ('forfiles /P "%%i" /s /m *.* /c "cmd /c echo @relpath"') do (
+			set "file=%%~A"
+			set "ext=%%~xA"
+			setlocal enableDelayedExpansion
+			set "file=!file:~2!"
+			
+			set first_character=!file:~0,1!
+			if NOT "!first_character!" == "#" (
+					
+				echo Writing file "%~dpnx2\!file!"		
+				if "%3" == "1" (
+					if !ext! == .dml (
+						if not exist "%~dpnx2\!file!" (
+							echo new DML file - writing DML1 header
+							echo DML1> "%~dpnx2\!file!"
+						)
 					)
 				)
+				md "%~dpnx2\!file!\.." 2>NUL
+				
+				if exist "%~dpnx2\!file!" (
+					echo: >> "%~dpnx2\!file!"
+					echo: >> "%~dpnx2\!file!"
+				)
+				
+				type "%%i\!file!" >> "%~dpnx2\!file!"
 			)
-			md "%~dpnx2\!file!\.." 2>NUL
-			
-			if exist "%~dpnx2\!file!" (
-				echo: >> "%~dpnx2\!file!"
-				echo: >> "%~dpnx2\!file!"
-			)
-			
-			type "%%i\!file!" >> "%~dpnx2\!file!"
-		)
-		endlocal
+			endlocal
+		)	
 	)
+	endlocal
 )
 
 cd %back%
@@ -90,40 +121,48 @@ mkdir "%~dpnx2"
 
 ::for each version folder, copy over the $common folder first, and then copy across that versions files
 for /D %%i in ("%~dpnx1\*") do (
-	echo %%i
-	echo %%~ni
 	
-	if NOT %%~ni == $common (
-	
-		::create version dir
-		md "%~dpnx2\%%~ni" 2>NUL
+	setlocal enableDelayedExpansion
+	set "folder_name=%%~nxi"
+	if "!folder_name:~0,1!" == "#" (
+		echo skipping version !folder_name!
+	) else (		
+		if NOT %%~ni == $common (
 		
-		::copy over $common stuff
-		if exist "%~dpnx1\$common" (
-			xcopy /Q /E /I "%~dpnx1\$common" "%~dpnx2\%%~ni"
-		)
+			echo Creating Version "%%~ni"
 		
-		::This is some hacked together magic
-		setlocal disableDelayedExpansion
-		for /f "delims=" %%A in ('forfiles /P "%%i" /s /m *.* /c "cmd /c echo @relpath"') do (
-			set "file=%%~A"
-			set "ext=%%~xA"
-			setlocal enableDelayedExpansion
-			set "file=!file:~2!"
-					
-			echo Writing file "%~dpnx2\%%~ni\!file!"
-				
-			md "%~dpnx2\%%~ni\!file!\.." 2>NUL
+			::create version dir
+			md "%~dpnx2\%%~ni" 2>NUL
 			
-			if exist "%~dpnx2\%%~ni\!file!" (
-				echo: >> "%~dpnx2\%%~ni\!file!"
-				echo: >> "%~dpnx2\%%~ni\!file!"
+			::copy over $common stuff
+			if exist "%~dpnx1\$common" (
+				echo Copying files from $common into new version %%~ni
+				xcopy /V /E /I "%~dpnx1\$common" "%~dpnx2\%%~ni" >NUL
 			)
 			
-			type "%%i\!file!" >> "%~dpnx2\%%~ni\!file!"
-			endlocal
+			::This is some hacked together magic
+			setlocal disableDelayedExpansion
+			for /f "delims=" %%A in ('forfiles /P "%%i" /s /m *.* /c "cmd /c echo @relpath"') do (
+				set "file=%%~A"
+				set "ext=%%~xA"
+				setlocal enableDelayedExpansion
+				set "file=!file:~2!"
+						
+				echo Writing file "%~dpnx2\%%~ni\!file!"
+					
+				md "%~dpnx2\%%~ni\!file!\.." 2>NUL
+				
+				if exist "%~dpnx2\%%~ni\!file!" (
+					echo: >> "%~dpnx2\%%~ni\!file!"
+					echo: >> "%~dpnx2\%%~ni\!file!"
+				)
+				
+				type "%%i\!file!" >> "%~dpnx2\%%~ni\!file!"
+				endlocal
+			)
 		)
 	)
+	endlocal
 )
 
 cd %back%
